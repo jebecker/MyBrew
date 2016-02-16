6987da56f4c7db2b3fe73d11b281b3e10648a4e4
Jayme Becker <beckerjayme@gmail.com>
2016-02-16 16:33:59 -0600

    I removed the tab bar controller and replaced it with a Page View Controller and set that up so it works with the 3 views we have. I embedded navigation controllers into all of the views. I also added view controller files for the My Beer view, Main Page View, and the Recommend view.

---
+++ MyBrew/MainPageViewController.swift
@@ -0,0 +1,88 @@
+//
+//  MainPageViewController.swift
+//  MyBrew
+//
+//  Created by Jayme Becker on 2/16/16.
+//  Copyright © 2016 Jayme Becker. All rights reserved.
+//
+
+import UIKit
+
+
+class MainPageViewController: UIPageViewController {
+    
+    //create the array of view controllers in the order I want them in
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController("MyBeer"),
+            self.newViewController("Discover"),
+            self.newViewController("Recommend")]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                direction: .Forward,
+                animated: true,
+                completion: nil)
+        }
+    }
+    
+    //instantiate the view controllers
+    private func newViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(name)ViewController")
+    }
+}
+
+//MARK: UIPageViewControllerDataSource
+extension MainPageViewController: UIPageViewControllerDataSource {
+        
+        func pageViewController(pageViewController: UIPageViewController,
+            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+                
+                guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                    return nil
+                }
+                
+                let previousIndex = viewControllerIndex - 1
+                
+                guard previousIndex >= 0 else {
+                    return nil
+                }
+                
+                guard orderedViewControllers.count > previousIndex else {
+                    return nil
+                }
+                
+                return orderedViewControllers[previousIndex]
+        }
+        
+        func pageViewController(pageViewController: UIPageViewController,
+            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+                
+                guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+                    return nil
+                }
+                
+                let nextIndex = viewControllerIndex + 1
+                let orderedViewControllersCount = orderedViewControllers.count
+                
+                guard orderedViewControllersCount != nextIndex else {
+                    return nil
+                }
+                
+                guard orderedViewControllersCount > nextIndex else {
+                    return nil
+                }
+                
+                return orderedViewControllers[nextIndex]
+        }
+        
+}
+
+
+
